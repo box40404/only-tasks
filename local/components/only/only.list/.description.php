55c33c5149f23fb506f3f2d0e628c3faf616d5f7
box40404 <pashayashik@mail.ru>
2026-05-08 20:48:05 +0300

    t6 upd some ui

---
+++ local/components/only/only.list/.description.php
@@ -0,0 +1,14 @@
+<?php
+$arComponentDescription = array(
+	"NAME" => "Список онли",
+	"DESCRIPTION" => "COMP_DESCR",
+	"PATH" => array(
+		"ID" => "content",
+		"CHILD" => array(
+			"ID" => "news",
+			"NAME" => "Новости"
+		)
+	),
+	"CACHE_PATH" => "Y",
+);
+?>
